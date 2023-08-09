bc0c24a0ce1d3a3767dca89c3664b59d1d45e50b
moisture824 <kevinjmoy@gmail.com>
2023-08-09 18:07:52 -0400

    work in progress - to be finished on Aug 10

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,19 @@ let pokemonList =[
     {name: "Charmander", height: 12, type: "fire"},
     {name: "Blastoise", height: 14, type: "water"},
 ];
+
+// This code uses a for loop to iterate over each item in a list called pokemonList
+for (let i=0; i <pokemonList.length; i++) {
+// For each item in the list, the code creates a variable called pokemon and assigns it the value of the current item
+    let pokemon = pokemonList[i];
+    document.write(pokemon.name + "<br>");
+// If the height is greater than or equal to 14, the code writes the Pokémon's name, height, and the label "Wow that's big!" on the website's DOM
+    if (pokemon.height >= 14) {
+        document.write(pokemon.name + " (height: " + pokemon.height + ") Wow that's big!");
+    } else {
+    // If the height is less than 14, only the Pokémon's name and height are written on the website's DOM
+        document.write(pokemon.name + " (height: " + pokemon.height + ")");
+        }
+    }
+
+//This is where I left off yesterday
