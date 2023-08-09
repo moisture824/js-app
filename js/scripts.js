e058c390979648d736ac7a06a1fccf3b4f761fff
moisture824 <kevinjmoy@gmail.com>
2023-08-09 10:12:25 -0400

    File relocation to new repository

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+//The push below allows the set to add multiple objects.
+
+let pokemonList =[];
+
+pokemonList.push ({
+    name: 'Pikachu',
+    height: 10,
+    type: 'lightening'
+})
+
+pokemonList.push ({
+    name: 'Charmander',
+    height: 12,
+    type: 'fire'
+})
+
+pokemonList.push ({
+    name: 'Blastoise',
+    height: 14,
+    type: 'water'
+});
