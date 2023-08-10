89cc2e48ac7676c11a15df12d6c77399bfbc1e9c
moisture824 <kevinjmoy@gmail.com>
2023-08-10 10:56:54 -0400

    Add additional space between Pokemon in list display

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,12 @@ let pokemonList =[
 for (let i=0; i <pokemonList.length; i++) {
 // For each item in the list, the code creates a variable called pokemon and assigns it the value of the current item
     let pokemon = pokemonList[i];
-    document.write(pokemon.name + "<br>");
+   
 // If the height is greater than or equal to 14, the code writes the Pokémon's name, height, and the label "Wow that's big!" on the website's DOM
     if (pokemon.height >= 14) {
-        document.write(pokemon.name + " (height: " + pokemon.height + ") Wow that's big!");
+        document.write(pokemon.name + " (height: " + pokemon.height +  ") - Wow that's big!<br><br>");
     } else {
     // If the height is less than 14, only the Pokémon's name and height are written on the website's DOM
-        document.write(pokemon.name + " (height: " + pokemon.height + ")");
+        document.write(pokemon.name + " (height: " + pokemon.height + ")<br><br>");
         }
     }
-
-//This is where I left off yesterday
