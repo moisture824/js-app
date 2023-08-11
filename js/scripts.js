e2267e6d70d4394497e69debe2db4c36caa65859
moisture824 <kevinjmoy@gmail.com>
2023-08-11 11:18:42 -0400

    Add IIFE to pokemonList
    Wrap code in an IIFE for easier code maintenance.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
 //{used for arrays} while [used for objects]
 
-let pokemonList =[
-    {name: "Pikachu", height: 10, type: "lightening"},
-    {name: "Charmander", height: 12, type: "fire"},
-    {name: "Blastoise", height: 14, type: "water"},
-];
+(function () {
+    let pokemonList =[
+        {name: "Pikachu", height: 10, type: "lightening"},
+        {name: "Charmander", height: 12, type: "fire"},
+        {name: "Blastoise", height: 14, type: "water"},
+    ];
 
-// Use the forEach() function to iterate over each element in the pokemonList array
-pokemonList.forEach(function(pokemon) {
-    // Check if the height of the current pokemon is greater than or equal to 14
-    if (pokemon.height >= 14) {
-        // If the height is greater than or equal to 14, write the name and height of the pokemon to the document, along with a message indicating that it is big
-        document.write(pokemon.name + " (height: " + pokemon.height + ") - Wow that's big!<br><br>");
-    } else {
-        // If the height is less than 14, write only the name and height of the pokemon to the document
-        document.write(pokemon.name + " (height: " + pokemon.height + ")<br><br>");
-    }
-});
+    // Use the forEach() function to iterate over each element in the pokemonList array
+    pokemonList.forEach(function(pokemon) {
+        // Check if the height of the current pokemon is greater than or equal to 14
+        if (pokemon.height >= 14) {
+            // If the height is greater than or equal to 14, write the name and height of the pokemon to the document, along with a message indicating that it is big
+            document.write(pokemon.name + " (height: " + pokemon.height + ") - Wow that's big!<br><br>");
+        } else {
+            // If the height is less than 14, write only the name and height of the pokemon to the document
+            document.write(pokemon.name + " (height: " + pokemon.height + ")<br><br>");
+        }
+    });
+})();
