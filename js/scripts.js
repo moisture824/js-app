5b605535d919d801fb8eadefe62e5f1f6f4ae86c
moisture824 <kevinjmoy@gmail.com>
2023-08-13 17:14:52 -0400

    Fix - added getAll function to access pokemonList outside IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -23,14 +23,14 @@ let pokemonRepository = (function () {
 
 })();
 
-    // Use the forEach() function to iterate over each element in the pokemonList array
-    pokemonList.forEach(function(pokemon) {
-        // Check if the height of the current pokemon is greater than or equal to 14
-        if (pokemon.height >= 14) {
-            // If the height is greater than or equal to 14, write the name and height of the pokemon to the document, along with a message indicating that it is big
-            document.write(pokemon.name + " (height: " + pokemon.height + ") - Wow that's big!<br><br>");
-        } else {
-            // If the height is less than 14, write only the name and height of the pokemon to the document
-            document.write(pokemon.name + " (height: " + pokemon.height + ")<br><br>");
-        }
-    });
+// Use the forEach() function to iterate over each element in the pokemonList array
+pokemonRepository.getAll().forEach(function(pokemon) {
+// Check if the height of the current pokemon is greater than or equal to 14
+    if (pokemon.height >= 14) {
+// If the height is greater than or equal to 14, write the name and height of the pokemon to the document, along with a message indicating that it is big
+        document.write(pokemon.name + " (height: " + pokemon.height + ") - Wow that's big!<br><br>");
+    } else {
+// If the height is less than 14, write only the name and height of the pokemon to the document
+        document.write(pokemon.name + " (height: " + pokemon.height + ")<br><br>");
+    }
+});
