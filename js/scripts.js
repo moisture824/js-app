6a5ac21f97a1d65f9749b51f0a65e3273672f1e0
moisture824 <kevinjmoy@gmail.com>
2023-08-14 11:50:35 -0400

    Add addListItem function to add new Pokemon to list
    The addListItem function takes in a new pokemon object, creates a new list & button, and appends the button to the list item & vice versa.

--- js/scripts.js
+++ js/scripts.js
@@ -7,30 +7,35 @@ let pokemonRepository = (function () {
         {name: "Blastoise", height: 14, type: "water"},
     ];
 
-// Define a function named getAll that returns the pokemonList array
+// Define a function named getAll that returns the pokemonList array.
     function getAll() {
         return pokemonList;
     }
-// Define a function named add that takes a single argument, pokemon, and adds it to the pokemonList array
+// Define a function named add that takes a single argument, pokemon, and adds it to the pokemonList array.
     function add(Snorlax) {
         pokemonList.push(Snorlax);
     }
-// Return an object with two properties, getAll and add, which are assigned the values of the getAll and add functions, respectively
+/* This function adds a new Pokemon to an existing list by creating a new list item and button, 
+setting the button text to the Pokemon's name, and appending the button to the list item and the list item to the list.*/
+    function addListItem(pokemon){
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listPokemon = document.createElement("li");
+        let button = document.createElement("button");
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        listPokemon.appendChild(button);
+        pokemonList.appendChild(listpokemon);
+    }
+// Return an object with three properties, getAll, add, & addListItem.
     return {
         getAll: getAll,
-        add: add
+        add: add,
+        addListItem: addListItem
     };
 
 })();
 
-// Use the forEach() function to iterate over each element in the pokemonList array
+// This code retrieves all Pokemon from the pokemonRepository, and for each Pokemon, adds a new list item to the list by calling the addListItem method of the pokemonRepository.
 pokemonRepository.getAll().forEach(function(pokemon) {
-// Check if the height of the current pokemon is greater than or equal to 14
-    if (pokemon.height >= 14) {
-// If the height is greater than or equal to 14, write the name and height of the pokemon to the document, along with a message indicating that it is big
-        document.write(pokemon.name + " (height: " + pokemon.height + ") - Wow that's big!<br><br>");
-    } else {
-// If the height is less than 14, write only the name and height of the pokemon to the document
-        document.write(pokemon.name + " (height: " + pokemon.height + ")<br><br>");
-    }
+    pokemonRepository.addListItem(pokemon);
 });
