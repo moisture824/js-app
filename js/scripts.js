a6b9ba2b2de6a05d550d0df650b0ae9b39d3ace6
moisture824 <kevinjmoy@gmail.com>
2023-08-16 18:18:39 -0400

    Add form validation for email & password

--- js/scripts.js
+++ js/scripts.js
@@ -100,3 +100,60 @@ pokemonRepository.loadList().then(function() {
         pokemonRepository.addListItem(pokemon);
     });
 });
+
+// To validate email
+(function validateEmail() {
+    let value = emailInput.value;
+    let hasAtSign = value.indexOf('@') > -1;
+    let hasDot =  value.indexOf('.') > -1;
+    return value && hasAtSign && hasDot;
+})();
+
+// To validate password
+(function validatePassword() {
+    let value = passwordInput.value;
+    return value && value.length >= 8;
+})();
+
+// Message displayed if there is an error
+(function showErrorMessage(input, message) {
+    let container = input.parentElement; // The .input-wrapper
+  
+    // Check and Remove any existing errors
+    let error = container.querySelector('.error-message');
+    if (error) {
+      container.removeChild(error);
+    }
+  
+    // Now add the error if the message isn’t empty
+    if (message) {
+      let error = document.createElement('div');
+      error.classList.add('error-message');
+      error.innerText = message;
+      container.appendChild(error);
+    }
+  })();
+
+  // These are error message templates
+(function validateEmail() {
+    let value = emailInput.value;
+  
+    if (!value) {
+      showErrorMessage(emailInput, 'Email is a required field.');
+      return false;
+    }
+  
+    if (value.indexOf('@') === -1) {
+      showErrorMessage(emailInput, 'You must enter a valid email address.');
+      return false;
+    }
+  
+    if (value.indexOf('.') === -1) {
+      showErrorMessage(emailInput, 'You must enter a valid email address.');
+      return false;
+    }
+  
+  
+    showErrorMessage(emailInput, null);
+    return true;
+})();
